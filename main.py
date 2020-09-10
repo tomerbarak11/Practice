6583af0d61ac9cb339d0ebabae0d987b59886b5c
unknown <=>
2020-09-10 17:46:06 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+def say_hello(username):
+  if auth():
+    print(f"Hello {username}")
+  else:
+    print(f"Hello guest, please log in.")
+
+def main():
+  try:
+    from my_top_secret_data import auth
+    username = input("What is your username?\n")
+    say_hello(username)
+  except:
+    print("Missing authentication module")
+
+if __name__ == "__main__":
+  main()
+  
